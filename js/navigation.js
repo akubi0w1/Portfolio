ce5082a7e7b49ca8374b3e310089cc665042412e
84-persian <84.cat.persian@gmail.com>
2019-06-21 13:31:45 +0900

    update js

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,4 @@
 function toggleNavigation(){
-    console.log("navigation change");
     var menu = $("#hamburger-menu");
     var navButton = $(".toggle-nav");
     
@@ -16,3 +15,18 @@ function toggleNavigation(){
         navButton.html('<i class="fas fa-times"></i>');
     }
 }
+
+$(function(){
+    $("#hamburger-menu a").click(function(){
+        var menu = $("#hamburger-menu");
+        var navButton = $(".toggle-nav");
+        
+        if(menu[0].className.indexOf("open") > -1){
+            menu.slideUp();
+            // menu.hide("slide", {direction: "right"});
+            menu.removeClass("open");
+            navButton.html('<i class="fas fa-bars"></i>');
+        }
+    });
+    
+});
